35ea7622be9df4a718dc8ea5fc4ace620f3f34bc
heimp <cheimpel@gmail.com>
2016-12-05 23:09:04 -0500

    third commit

---
+++ langtable/isotopes.js
@@ -0,0 +1,33 @@
+$(function() {
+	console.log("yeehaw");
+
+	var grid = $('.grid');
+
+	grid.isotope({
+		// options
+		itemSelector: '.tile',
+		layoutMode: 'fitRows',
+		getSortData: {
+			name: function(element) {
+				return $(element).find('.name').text().toLowerCase();
+			}
+		}
+	});
+
+	$('.filter-button-group').on( 'click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		grid.isotope({ filter: filterValue });
+	});
+
+	$('.alphabetical-sort').on('click', function() {
+		grid.isotope({ sortBy: 'name' });
+	})
+
+	$('.search-name').on('input', function() {
+		var text = $(this).val();
+		grid.isotope({filter: function() {
+			var name = $(this).find('.name').text();
+			return name.match(new RegExp(text, 'i'));
+		}});
+	});
+});
